c9faa5cf23f2a8e49fe2fd90ea6d4dd55bcde088
Eoghan Lappin <eoghanlappin@gmail.com>
2013-02-11 21:59:32 +0000

    [Bash] Bash script to clear up Oracle .trm and .trc files.

--- easy2.sh
+++ easy2.sh
@@ -6,12 +6,12 @@
 cd opt/app/oracle/oradata/orcl/trace/
 
 #Remove all trc files in the directory older than 10 days.
-find . -name "*.trc" -mtime +35 -exec rm -rf  {} \;
+find . -name "*.trc" -mtime +10 -exec rm -rf  {} \;
 
 echo "Removed all .trc files older than 10 days..."
 
 #Remove all trm files in the directory older than 10 days.
-find . -name "*.trm" -mtime +35 -exec rm -rf  {} \;
+find . -name "*.trm" -mtime +10 -exec rm -rf  {} \;
 
 echo "Removed all .trm files older than 10 days..."
 
