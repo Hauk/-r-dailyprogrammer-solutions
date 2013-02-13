77b5c5cb9e761dc984b87a0b4cf973f8c45a5329
Eoghan Lappin <eoghanlappin@gmail.com>
2013-02-13 10:15:32 +0000

    [Bash] Updated to reflect actual directories and working commands.

--- easy2.sh
+++ easy2.sh
@@ -3,15 +3,15 @@
 #Untested but I'm sure it's fine!
 
 # CD to the Oracle trace directory - Change to actual directory location.
-cd opt/app/oracle/oradata/orcl/trace/
+cd /opt/app/oracle/diag/rdbms/orcl/orcl/trace/
 
 #Remove all trc files in the directory older than 10 days.
-find . -name "*.trc" -mtime +10 -exec rm -rf  {} \;
+find . -name \*.trc -mtime +10 -exec rm {} \;
 
 echo "Removed all .trc files older than 10 days..."
 
 #Remove all trm files in the directory older than 10 days.
-find . -name "*.trm" -mtime +10 -exec rm -rf  {} \;
+find . -name \*.trc -mtime +14 -exec rm {} \;
 
 echo "Removed all .trm files older than 10 days..."
 
